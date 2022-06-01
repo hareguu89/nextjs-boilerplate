c072ce0be501506c16aec419c829d0b0e68113c1
jhpark <hareguu89@gmail.com>
2022-06-01 21:36:16 +0900

    Update setup styled-components with type

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
-import { useRecoilState } from 'recoil'
-import { fontSizeState } from '../store'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { fontSizeState } from '../src/store'
+import styled from 'styled-components'
 
-const FontButton = () => {
+const FontButton = (): JSX.Element => {
 	const [fontSize, setFontSize] = useRecoilState(fontSizeState)
+
 	return (
 		<button
 			onClick={() => setFontSize((size) => size + 1)}
@@ -16,24 +18,42 @@ const FontButton = () => {
 	)
 }
 
-const Text = () => {
-	const [fontSize, setFontSize] = useRecoilState(fontSizeState)
+const Text: React.FC<{ text: string }> = ({ text }): JSX.Element => {
+	const fontSize = useRecoilValue(fontSizeState)
+
+	useEffect(() => {
+		console.log(text)
+	}, [text])
+
 	return <p style={{ fontSize }}>This text will increase in size too.</p>
 }
 
-const Home: NextPage = () => {
-	const [counter, setCoutner] = useState(0)
+const Home: NextPage = (): JSX.Element => {
+	const [counter, setCoutner] = useState<number>(0)
+	const [inputTest, setInputTest] = useState<string>('')
+
+	const changeEvent = ({
+		target: { value },
+	}: ChangeEvent<HTMLInputElement>): void => {
+		setInputTest(value)
+	}
 
 	return (
 		<>
 			<div className={styles.container}>
 				{counter}
-				<Text />
+				<Text text={inputTest} />
 				<FontButton />
 				<button onClick={() => setCoutner((pre) => pre + 1)}> click </button>
+				<Input type="text" value={inputTest} onChange={changeEvent} />
+				{inputTest}
 			</div>
 		</>
 	)
 }
+
+const Input = styled.input`
+	padding: 10px;
+`
 
 export default Home
