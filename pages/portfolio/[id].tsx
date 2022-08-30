51408cd4b9d34d8579f40d6d97589919e2f8802b
jhpark <hareguu89@gmail.com>
2022-08-30 13:43:17 +0900

    Update dynamic routing

---
+++ pages/portfolio/[id].tsx
@@ -0,0 +1,23 @@
+import { NextPage } from 'next'
+import { useRouter, NextRouter } from 'next/router'
+// interface PortfolioProjectPageProps {}
+
+const PortfolioProjectPage: NextPage = () => {
+	// To access dynamic segment value
+	const router: NextRouter = useRouter()
+
+	console.log('router.pathname :', router.pathname)
+	console.log('router.query:', router.query)
+	// send a request to some backend server
+	// to fetch the piece of data with an id of router.query.id
+
+	return (
+		<>
+			<div>project page</div>
+			<div>{router.query?.id}</div>
+			<div>{router.pathname}</div>
+		</>
+	)
+}
+
+export default PortfolioProjectPage
