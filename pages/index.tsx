7be3ce87cbc46507ade3b1f1de500196b0276ef4
Jung Hwan <admin@MacBook-Pro.local>
2023-02-01 22:39:37 +0900

    scale point

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,59 +8,59 @@ import path from 'path';
 // import { useEffect } from 'react';
 
 const Home = ({ products }: InferGetStaticPropsType<typeof getStaticProps>) => {
-	// const [products, setProducts] = useState<IDummyBackend[]>([]);
-	// console.log(products);
-	// useEffect(() => {
-	//   const test = async () => {
-	//     await axios.get<IResponse>('/dummy-backend.json').then(res => {
-	//       setProducts(res.data.product);
-	//     });
-	//   };
-	//   test();
-	// }, []);
+  // const [products, setProducts] = useState<IDummyBackend[]>([]);
+  // console.log(products);
+  // useEffect(() => {
+  //   const test = async () => {
+  //     await axios.get<IResponse>('/dummy-backend.json').then(res => {
+  //       setProducts(res.data.product);
+  //     });
+  //   };
+  //   test();
+  // }, []);
 
-	return (
-		<>
-			<Seo title={'Home'} />
-			<Sidebar />
+  return (
+    <>
+      <Seo title={'Home'} />
+      <Sidebar />
 
-			<Link href="/login">path</Link>
-			<ul>
-				{products.map((product: IDummyBackend) => (
-					<Link href={`/${product.id}`} key={product.id}>
-						{product.title}
-					</Link>
-				))}
-			</ul>
-		</>
-	);
+      <Link href="/login">path</Link>
+      <ul>
+        {products.map((product: IDummyBackend) => (
+          <Link href={`/${product.id}`} key={product.id}>
+            {product.title}
+          </Link>
+        ))}
+      </ul>
+    </>
+  );
 };
 
-export const getStaticProps: GetStaticProps = async (context) => {
-	console.log(context);
-	console.log('re-generated');
-	const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
-	const jsonData = await fs.readFile(filePath);
-	const data = JSON.parse(jsonData.toString());
+export const getStaticProps: GetStaticProps = async context => {
+  console.log(context);
+  console.log('re-generated');
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData.toString());
 
-	if (!data.products.length) {
-		// if fetching is failed => 404 page shown
-		return { notFound: true };
-	}
+  if (!data.products.length) {
+    // if fetching is failed => 404 page shown
+    return { notFound: true };
+  }
 
-	if (!data) {
-		return {
-			redirect: { destination: '/' },
-			props: {},
-		};
-	}
+  if (!data) {
+    return {
+      redirect: { destination: '/' },
+      props: {},
+    };
+  }
 
-	return {
-		props: {
-			products: data.products,
-		},
-		revalidate: 3, // ISR(incremental static regeneration) - x per sec, regenerate the pages,
-	};
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 3, // ISR(incremental static regeneration) - x per sec, regenerate the pages,
+  };
 };
 
 export default Home;
